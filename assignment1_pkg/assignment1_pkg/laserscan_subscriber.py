cdb2bb4f597ced2659961f332c715f8ac3e4aecf
daimen2210 <daimen2210@gmail.com>
2025-09-10 12:36:22 +0200

    qos settings

--- assignment1_pkg/assignment1_pkg/laserscan_subscriber.py
+++ assignment1_pkg/assignment1_pkg/laserscan_subscriber.py
@@ -30,4 +30,3 @@ def main(args=None):
 if __name__ == '__main__':
     main()
 
-
--- build/assignment1_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/daimen/Documents/TEL280_assignments/install/assignment1_pkg'
