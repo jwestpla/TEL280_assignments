2421271cf4b85877eed0037f687b13c6d986ee0b
Joep <joepwestplate@gmail.com>
2025-09-15 13:53:35 +0000

    Added launch folder

---
+++ assignment1_pkg/assignment1_pkg/assignment1_pkg/laserscan_subscriber.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, ReliabilityPolicy
+from sensor_msgs.msg import LaserScan
+
+class LaserscanSubscriber(Node):
+    def __init__(self):
+        super().__init__("laserscan_subscriber")
+        self.subscription = self.create_subscription(
+            LaserScan, #message
+            "/scan", #topic
+            self.listener_callback,
+            QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT) # ros2 topic info /scan -v
+        )
+    
+    def listener_callback(self, msg):
+        distances = msg.ranges
+        if any(dist < 1 for dist in distances): 
+            self.get_logger().info('Obstacle')
+        else:
+            self.get_logger().info('Free')
+
+def main(args=None):
+    rclpy.init(args = args)
+    node = LaserscanSubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
